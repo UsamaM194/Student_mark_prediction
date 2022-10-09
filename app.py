b97aa15dd0ce9610a72ff65407329b893ed9e0b0
ICRL Python <113352684+UsamaM194@users.noreply.github.com>
2022-10-09 12:09:23 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from cgitb import html
+from pyexpat import features, model
+from turtle import home
+from xml.sax.handler import feature_string_interning
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("std_mark_predictor_model.pkl") 
+
+df = pd.DataFrame()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    global df
+
+    input_features = [int(x) for x in request.form.values()]
+    features_value = np.array(input_features)
+
+    # Validate input hours 
+    if input_features[0] <0 or input_features[0]>24:
+        return render_template('index.html', prediction_text='Plz enter a vaild hour between 1 to 24')
+        
+    output = model.predict([features_value])[0][0].round(2)
+ # input and predict values store in df then save in csv file
+    df = pd.concat([df, pd.DataFrame({'Study Hours':  input_features, 'Predicted Output': [output]})],ignore_index=True)
+    print(df)
+    df.to_csv('smp_data_from_app.csv')
+
+    return render_template('index.html', prediction_text='You will get [{}%] marks, when you do study[{}] hours per day'.format(output, int(features_value[0])))
+if __name__ == "__main__":
+    app.run()
+
+ 
